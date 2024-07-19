b457067a54c27a2a1a9c71b81fd970e7d451eee4
Jade-fu <32590310+richerfu@users.noreply.github.com>
2024-07-19 09:49:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.qinglian-studio.com": dockerHub,
+  "quay.qinglian-studio.com": "https://quay.io",
+  "gcr.qinglian-studio.com": "https://gcr.io",
+  "k8s-gcr.qinglian-studio.com": "https://k8s.gcr.io",
+  "k8s.qinglian-studio.com": "https://registry.k8s.io",
+  "ghcr.qinglian-studio.com": "https://ghcr.io",
+  "cloudsmith.qinglian-studio.com": "https://docker.cloudsmith.io",
+  "ecr.qinglian-studio.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.qinglian-studio.com": dockerHub,
 };
 
 function routeByHosts(host) {
